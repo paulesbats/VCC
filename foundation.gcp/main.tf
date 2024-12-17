c5bc94a0f47965e338a18b80de70cdb974bf5f72
paulesbats <paullesbats2003@gmail.com>
2024-12-17 11:14:13 +0100

    VPC

--- foundation.gcp/main.tf
+++ foundation.gcp/main.tf
@@ -5,3 +5,13 @@ provider "google"{
     credentials = file("student.json")
 }
 
+resource "google_compute_network" "vpc_network" {
+    name = "vpc-network"
+}
+
+resource "google_compute_subnetwork" "vpc_subnetwork" {
+    name = "vpc-subnetwork"
+    ip_cidr_range = "192.168.1.0/24"
+    network = google_compute_network.vpc_network.name
+}
+
