8eb062831a11c5e25b522c2468d2ee1594a3c930
paulesbats <paullesbats2003@gmail.com>
2024-12-17 14:42:19 +0100

    DNS

--- foundation.gcp/main.tf
+++ foundation.gcp/main.tf
@@ -48,3 +48,17 @@ resource "google_sql_database" "database" {
     name = "database"
     instance = google_sql_database_instance.database_instance.name
 }
+
+resource "google_dns_managed_zone" "dns_zone" {
+    name = "dns-zone"
+    dns_name = "dns.mydomain.com."
+}
+
+resource "google_dns_record_set" "server" {
+    name = "server.${google_dns_managed_zone.dns_zone.dns_name}"
+    type = "A"
+    ttl = 300
+    managed_zone = google_dns_managed_zone.dns_zone.name
+    rrdatas = [ google_compute_instance.server_instance.network_interface[0].access_config[0].nat_ip ]
+}
+
